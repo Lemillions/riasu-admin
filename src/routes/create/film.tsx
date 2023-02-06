1bda606a5298fb6c76b722af2ee0e84bffe98ee6
Lemillions <marcosvsdxd@gmail.com>
2023-02-06 17:52:52 -0300

    finalizando pagina de filmes

--- src/routes/create/film.tsx
+++ src/routes/create/film.tsx
@@ -1,31 +1,121 @@
-import "../../styles/film.css"
-import axios from "axios"
-import { useState, useEffect } from "react"
+import "../../styles/film.css";
+import { useEffect, useState } from "react";
+import { api } from "../../api";
+import { Input, message, Select } from "antd";
+import { BiArrowBack } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 
-interface Film {
-    name: string,
-    description?: string,
-    src: string,
-    banner?: string,
-    genres: string[],
-    products?: string[]
+interface Filme {
+  id: string;
+  name: string;
+  description?: string;
+  src: string;
+  banner: string;
+  genres: string[];
+  createdAt: string;
 }
-export default function FilmCreate(){
-  const [filmes, setFilmes] = useState<Film[]>([])
+export default function FilmCreate() {
+  const [listaGeneros, setListaGeneros] = useState<any[]>([]);
+  const [form, setForm] = useState<Filme>({
+    id: "",
+    name: "",
+    description: "",
+    src: "",
+    banner: "",
+    genres: [],
+    createdAt: "",
+  });
 
-  useEffect(()=>{
-    axios.get('http://localhost:3333/api/film')
-    .then((res)=> setFilmes(res.data))
-  },[])
-  
-  return(
-    <div id='filmContainer'>
-      <h1>Filmes</h1>
-      <div id="listaFilmes">
-        {filmes.map((filme)=> {
-          return <>{filme.name}</>
-        })}
+  useEffect(() => {
+    api
+      .get("genre")
+      .then((res) => setListaGeneros(res.data))
+      .catch((err) =>
+        message.error(
+          "Ocorreu um erro e não vai ser possivel selecionar os generos!"
+        )
+      );
+  });
+
+  const criarFilme = () => {
+    if(form.name.length && form.src.length && form.banner.length ){
+    api
+      .post("film", {
+        name: form.name,
+        description: form.description,
+        src: form.src,
+        banner: form.banner,
+      })
+      .then((res) => {
+        message.success("Filme criado com sucesso!");
+        api
+          .post(`film/${res.data.id}/genre`, form.genres)
+          .catch((err) =>
+            message.error("Ocorreu um erro ao adicionar generos!")
+          );
+
+        setForm({
+          id: "",
+          name: "",
+          description: "",
+          src: "",
+          banner: "",
+          genres: [],
+          createdAt: "",
+        });
+      })
+      .catch((err) => message.error("Ocorreu um erro ao criar o filme!"));
+    }else{
+      message.error("Preencha todos os campos obrigatórios!");
+    }
+  };
+
+  const navigate = useNavigate();
+
+  return (
+    <div id="filmContainer">
+      <header>
+        <BiArrowBack
+          size={26}
+          onClick={() => navigate(-1)}
+          style={{ cursor: "pointer" }}
+        />
+        <h1>Filmes</h1>
+      </header>
+      <div id="formContainer">
+        <label>* Nome :</label>
+        <Input
+          value={form.name}
+          onChange={(e) => setForm({ ...form, name: e.target.value })}
+        />
+        <label>Descrição :</label>
+        <Input
+          value={form.description}
+          onChange={(e) => setForm({ ...form, description: e.target.value })}
+        />
+        <label>* Link do filme :</label>
+        <Input
+          value={form.src}
+          onChange={(e) => setForm({ ...form, src: e.target.value })}
+        />
+        <label>* Link da imagem :</label>
+        <Input
+          value={form.banner}
+          onChange={(e) => setForm({ ...form, banner: e.target.value })}
+        />
+        <label>Generos :</label>
+        <Select
+          mode="multiple"
+          value={form.genres}
+          onChange={(e) => setForm({ ...form, genres: e })}
+          style={{ width: "100%" }}
+          options={listaGeneros.map((genero) => ({
+            label: genero.name,
+            value: genero.id,
+          }))}
+        />
+        <button onClick={criarFilme}>Criar filme</button>
       </div>
     </div>
-  )
+  );
 }
--- src/routes/film.tsx
+++ src/routes/film.tsx
@@ -1,31 +1,183 @@
-import "../styles/film.css"
-import axios from "axios"
-import { useState, useEffect } from "react"
+import "../styles/film.css";
+import { Input, Modal, Tooltip, Select, message } from "antd";
+import { MdOutlineAdd } from "react-icons/md";
+import { useEffect, useState } from "react";
+import { api } from "../api";
+import { useNavigate } from "react-router-dom";
 
-interface Film {
-    name: string,
-    description?: string,
-    src: string,
-    banner: string,
-    genres: string[],
-    products?: string[]
+interface Filme {
+  id: string;
+  name: string;
+  description?: string;
+  src: string;
+  banner: string;
+  genres: string[];
+  products?: string[];
+  createdAt: string;
 }
-export default function Film(){
-  const [filmes, setFilmes] = useState<Film[]>([])
+export default function Film() {
+  const [filmes, setFilmes] = useState<Filme[]>([]);
+  const [buscaInput, setBuscaInput] = useState<string>("");
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [listaGeneros, setListaGeneros] = useState<any[]>([]);
+  const [form, setForm] = useState<Filme>({
+    id: "",
+    name: "",
+    description: "",
+    src: "",
+    banner: "",
+    genres: [],
+    products: [],
+    createdAt: "",
+  });
 
-  useEffect(()=>{
-    axios.get('http://localhost:3333/api/film')
-    .then((res)=> setFilmes(res.data))
-  },[])
-  
-  return(
-    <div id='filmContainer'>
-      <h1>Filmes</h1>
-      <div id="listaFilmes">
-        {filmes.map((filme)=> {
-          return <>{filme.name}</>
-        })}
+  useEffect(() => {
+    api
+      .get("film")
+      .then((res) => setFilmes(res.data))
+      .catch((err) => console.log(err));
+
+    api
+      .get("genre")
+      .then((res) => setListaGeneros(res.data))
+      .catch((err) =>
+        message.error(
+          "Ocorreu um erro e não vai ser possivel selecionar os generos!"
+        )
+      );
+  });
+  const navigate = useNavigate();
+  const filmesFiltrados = buscaInput.length
+    ? filmes.filter((e) =>
+        e.name.toUpperCase().includes(buscaInput.toUpperCase())
+      )
+    : filmes;
+
+  const showModal = (filme: Filme) => {
+    setForm({ ...filme, genres: filme.genres.map((e: any) => e.genreId) });
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    api
+      .put(`film/${form.id}`, {
+        name: form.name,
+        description: form.description,
+        src: form.src,
+        banner: form.banner,
+      })
+      .then((res) => {
+        api
+          .post(`film/${res.data.id}/genre`, form.genres)
+          .catch((err) =>
+            message.error("Ocorreu um erro ao adicionar generos!")
+          );
+      })
+      .catch((err) => message.error("Ocorreu um erro ao editar o filme!"));
+
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div id="filmContainer">
+      <header>
+        <h1>Planos</h1>
+      </header>
+      <div id="produtosContainer">
+        <div id="inputsContainer">
+          <div style={{ width: "500px" }}>
+            <Input.Search
+              placeholder="Pesquisar Produtos"
+              onChange={(e) => setBuscaInput(e.currentTarget.value)}
+              size="large"
+            />
+          </div>
+          <button onClick={() => navigate("/film/create")}>
+            <MdOutlineAdd size={16} color="#fff" />
+            Criar Filme
+          </button>
+        </div>
+        <div id="listaFilmes">
+          <div id="headerLista">
+            <span className="filmeNome">Nome</span>
+            <span className="filmeDescricao">Descrição</span>
+            <span className="filmeData">Data de Criação</span>
+          </div>
+          <Modal
+            title="Atualizar filme"
+            open={isModalOpen}
+            okText={"Salvar alterações"}
+            cancelText={"Cancelar"}
+            onOk={handleOk}
+            onCancel={handleCancel}
+          >
+            <label>Nome :</label>
+            <Input
+              value={form.name}
+              onChange={(e) =>
+                setForm({ ...form, name: e.currentTarget.value })
+              }
+              style={{ marginBottom: "5px" }}
+            />
+            <label>Descrição :</label>
+            <Input
+              value={form.description}
+              onChange={(e) =>
+                setForm({ ...form, description: e.currentTarget.value })
+              }
+              style={{ marginBottom: "5px" }}
+            />
+            <label>Link do filme :</label>
+            <Input
+              value={form.src}
+              onChange={(e) => setForm({ ...form, src: e.currentTarget.value })}
+              style={{ marginBottom: "5px" }}
+            />
+            <label>Link da imagem :</label>
+            <Input
+              value={form.banner}
+              onChange={(e) =>
+                setForm({ ...form, banner: e.currentTarget.value })
+              }
+              style={{ marginBottom: "5px" }}
+            />
+            <label>Generos :</label>
+            <Select
+              mode="multiple"
+              style={{ width: "100%" }}
+              placeholder="Selecione os generos"
+              value={form.genres}
+              onChange={(e) => setForm({ ...form, genres: e })}
+              options={listaGeneros.map((genero) => {
+                return { label: genero.name, value: genero.id };
+              })}
+            />
+          </Modal>
+          {filmesFiltrados
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .map((filme: Filme) => {
+              return (
+                <div className="filmeInfo">
+                  <span
+                    className="filmeNome"
+                    onClick={() => showModal(filme)}
+                    style={{ color: "#3636ff", cursor: "pointer" }}
+                  >
+                    {filme.name}
+                  </span>
+                  <span className="filmeDescricao">{filme.description}</span>
+                  <span className="filmeData">
+                    {new Date(filme.createdAt).toLocaleDateString("pt-BR")}
+                  </span>
+                </div>
+              );
+            })}
+        </div>
       </div>
     </div>
-  )
+  );
 }
